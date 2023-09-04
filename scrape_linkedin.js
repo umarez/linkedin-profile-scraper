8c2fee39a98f39444ac0bafa72757b79292a32f7
umarez <umar.izzuddin@ui.ac.id>
2023-09-04 16:07:43 +0700

    save browser session

--- scrape_linkedin.js
+++ scrape_linkedin.js
@@ -2,33 +2,41 @@ const puppeteer = require("puppeteer");
 require("dotenv").config();
 
 let scrape = async () => {
-  const browser = await puppeteer.launch();
+  const URL = "https://linkedin.com";
+
+  const browser = await puppeteer.launch({
+    userDataDir: "./user_data",
+  });
   const page = await browser.newPage();
 
-  await page.goto("https://linkedin.com");
+  await page.goto(URL);
   console.log("go to linkedin.com success");
+  try {
+    await page
+      .locator("#session_key")
+      .setTimeout(3000)
+      .fill(process.env.LINKEDIN_LOGIN_EMAIL);
 
-  await page
-    .locator("#session_key")
-    .setTimeout(5000)
-    .fill(process.env.LINKEDIN_LOGIN_EMAIL);
+    await page.waitForTimeout(2000);
+    await page
+      .locator("#session_password")
+      .fill(process.env.LINKEDIN_LOGIN_PASSWORD);
+    await page.waitForTimeout(2000);
+    const loginButton = await page.waitForSelector(
+      '[data-id="sign-in-form__submit-btn"]',
+      {
+        timeout: 5000,
+      }
+    );
+    loginButton.click();
+    console.log("Signin In...");
+    await page.waitForTimeout(5000);
+    console.log("login success!");
+  } catch (err) {
+    console.log("already login")
+  }
 
-  await page.waitForTimeout(2000);
-  await page
-    .locator("#session_password")
-    .fill(process.env.LINKEDIN_LOGIN_PASSWORD);
-  await page.waitForTimeout(2000);
-  const loginButton = await page.waitForSelector(
-    '[data-id="sign-in-form__submit-btn"]',
-    {
-      timeout: 5000,
-    }
-  );
-  loginButton.click();
-  console.log("Signin In...");
-  await page.waitForTimeout(5000);
-  console.log("login success!");
-  await page.goto("https://www.linkedin.com/in/umar-izzuddin/");
+  await page.goto(`${URL}/in/umar-izzuddin/`);
 
   console.log("go to profile success");
   await page.waitForTimeout(3000);
@@ -52,9 +60,7 @@ let scrape = async () => {
 
   console.log("getting profile experiences...");
 
-  await page.goto(
-    "https://www.linkedin.com/in/umar-izzuddin/details/experience/"
-  );
+  await page.goto(`${URL}/in/umar-izzuddin/details/experience/`);
 
   await page.waitForTimeout(3000);
 
@@ -66,11 +72,19 @@ let scrape = async () => {
     const experienceLength = experienceElements.length;
 
     for (let i = 0; i < experienceLength; i++) {
-      experiences.push(
-        experienceElements[i].children[0].children[0].children[1].querySelector(
-          "span[aria-hidden='true']"
-        ).textContent
+      const exp = {};
+      const blockContent = experienceElements[
+        i
+      ].children[0].children[0].children[1].querySelectorAll(
+        "span[aria-hidden='true']"
       );
+
+      exp.title = blockContent[0].textContent;
+      exp.company = blockContent[1].textContent.split("·")[0];
+      exp.position = blockContent[1].textContent.split("·")[1];
+      exp.period = blockContent[2].textContent;
+
+      experiences.push(exp);
     }
 
     return experiences;
